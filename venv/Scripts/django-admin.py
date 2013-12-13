8819f6438d8e9e429530fb8b06bb63ed8ddaef0e
chemcnabb <chemcnabb@gmail.com>
2013-12-13 10:10:51 -0500

    Initial commit of Ultimate Screenwriter

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Che\Documents\Web Development\django_ultimate_screenwriter\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
